f1006583663d5cee2daf25e91d5478f49d6abf14
sumeetmishra1 <sumitsfs0@gmail.com>
2023-10-22 17:40:50 +0530

    till scale

--- scriptnew.js
+++ scriptnew.js
@@ -1,6 +1,7 @@
 const myForm=document.querySelector('#my-form');
        const nameInput=document.querySelector('#name');
        const emailInput=document.querySelector('#email');
+       const phoneInput=document.querySelector('#phone');
        const msg=document.querySelector('.msg');
        const userList=document.querySelector('#users');
        
@@ -8,17 +9,20 @@ const myForm=document.querySelector('#my-form');
        function onSubmit(e){
        let myoj={
        name:nameInput.value,
-       email:emailInput.value
+       email:emailInput.value,
+       phone:phoneInput.value
        };
        let myobj=JSON.stringify(myoj);
         e.preventDefault();
-        localStorage.setItem('myoj',myobj);
+        localStorage.setItem(emailInput.value,myobj);
+       
        //console.log(`Name=${nameInput.value} email=${emailInput.value}`);
-       //const li=document.createElement('li');
-       //li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value}`));
-       //userList.appendChild(li);
-       //nameInput.value='';
-       //emailInput.value='';
+       const li=document.createElement('li');
+       li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value} ${phoneInput.value}`));
+       userList.appendChild(li);
+       nameInput.value='';
+       emailInput.value='';
+       phoneInput.value='';
        }
        //  const ul=document.querySelector('.items');
        //  ul.firstElementChild.textContent='Hello';
